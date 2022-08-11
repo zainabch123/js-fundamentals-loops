edc71732b2450d5730c659781eeca9418c657531
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-08-11 11:55:13 +0100

    Nested loops extension

---
+++ src/extensions/nested-loops.js
@@ -0,0 +1,43 @@
+const START = 1
+const END = 10
+
+const simpleOne = []
+const nestedOne = []
+const nestedTwo = []
+const deepOne = []
+const deepTwo = []
+const deepThree = []
+
+// 1. Using a for loop from 1 to 10, add the value of (10 - 'loop index') to the array 'simpleOne'
+//    eg [1,2,...]
+
+// HINT: Loop needs to be changed
+for (let i = 5; i < 7; i++) {
+  // Your code here
+}
+
+// 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
+//    eg [[1],[2,2],...]
+
+// 3. As 2, but each array should contain the values from the outer 'loop index' to 0 inclusive. Update array 'nestedTwo'
+//    eg [[1],[2,1],...]
+
+// 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
+//    eg [[[1]],[[2],[2,2]],...]
+
+// 5. As 4, update array 'deepTwo' so that the result is:
+//       [[[1]],[[1],[1,2]],...]
+
+// 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
+//       [[1],[[1],[3]],...]
+
+module.exports = {
+  START,
+  END,
+  simpleOne,
+  nestedOne,
+  nestedTwo,
+  deepOne,
+  deepTwo,
+  deepThree
+}
