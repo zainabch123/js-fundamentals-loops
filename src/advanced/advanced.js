0b45661468fb27b25b9832a50e15c7e4f464e847
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-04-23 19:31:13 +0100

    Completed advanced task.

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -4,22 +4,76 @@ const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-l
 // note: use a break statement to exit the loop early if the value is found
 // to prove you have done this, set the variable indexOfTen to the iteration index when you find 10
 let hasTen = false
-let indexOfTen = -1
+let indexOfTen = 1
+
+for (let i = 0; i<nums.length; i++) {
+  if (nums[i] === 10) {
+    hasTen = true;
+    console.log(i);
+    break;
+    } else {
+      hasTen = false;
+    }
+}
+
+console.log(hasTen);
 
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
 let divisibleByThreeCount = 0
+for (let i = 0; i<= nums.length; i++) {
+  if (nums[i] % 3 == 0) {
+    divisibleByThreeCount = divisibleByThreeCount + 1;
+  }
+}
+  console.log(divisibleByThreeCount);
 
 // 3. use a for loop to find the average of the numbers in the array
 let average = 0
+for (let i = 0; i< nums.length; i++) {
+  average += nums[i];
+}
+average = average/nums.length;
+console.log(average);
 
 // 4. use a for loop to find the largest number in the array
 let largest = 0
+largestNum = 6
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] > largestNum) {
+    largestNum = nums[i];
+    largest = largestNum;
+  }
+}
+
+console.log(largest);
 
 // 5. use a for loop to find the smallest number in the array
 let smallest = 100000
 
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] < smallest) {
+    smallest = nums[i];
+  }
+}
+console.log(smallest);
+
 // 6. find the median of the numbers in the array
 let median = 0
+sortedNumsArray = [];
+sortedNumsArray = nums.sort((a, b) => a - b);
+console.log(sortedNumsArray);
+
+if (sortedNumsArray.length % 2 === 0) {
+  middleValue = sortedNumsArray.length / 2 - 1;
+} else {
+  middleValue = sortedNumsArray.length / 2;
+}
+console.log(middleValue);
+
+median = sortedNumsArray[middleValue];
+console.log(median);
+
 
 module.exports = {
   hasTen,
--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -11,6 +11,12 @@ const deepThree = []
 // 1. Using a for loop from 1 to 10, add the value of the 'loop index' to the array 'simpleOne'
 //    eg [1,2,3...]
 
+for (let i = 1; i <= 10; i++) {
+  simpleOne.push(i);
+}
+console.log(simpleOne);
+
+
 // HINT: in the below loop, the statements and block of code needs to be changed
 // HINT: in the below loop, the var i represents the loop index
 // for (let i = 5; i < 7; i++) {
@@ -20,8 +26,27 @@ const deepThree = []
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
 
+for (let i = 1; i<=simpleOne.length; i++) {
+  let innerArray = []
+  for (j = 1; j<= i; j++) {
+    innerArray.push(i);
+  }
+  nestedOne.push(innerArray);
+}
+console.log(nestedOne);
+
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
+
+for (let i = 1; i<=simpleOne.length; i++) {
+let innerArray = []
+  for (j = 1; j<= i; j++) {
+    innerArray.push(i);
+  }
+  nestedTwo.push(innerArray);
+}
+console.log(nestedTwo);
+
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
